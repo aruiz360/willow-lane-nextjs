5023f3d271f4f86ccb7eb208214df8a9ea686428
Andres Ruiz <a.ruiz@axiomaestudio.com>
2025-09-16 16:12:06 +0200

    Added Press Release

--- src/app/press-releases/page.js
+++ src/app/press-releases/page.js
@@ -9,9 +9,16 @@ const slides = [
   {
     id: 1,
     image: '/press/bloomberg.jpg',
-    title: "Bloomberg Article",
-    description: "Boost Run Set to Go Public in US Via $614 Million SPAC Merger.",
+    title: "Boost Run Set to Go Public in US Via $614 Million SPAC Merger",
+    description: "AI cloud infrastructure company Boost Run LLC has reached an agreement to go public on the Nasdaq through a merger with Willow Lane Acquisition Corp.",
     link: "https://www.bloomberg.com/news/articles/2025-09-16/boost-run-set-to-go-public-in-us-via-614-million-spac-merger?srnd=undefined&embedded-checkout=true",
+  },
+  {
+    id: 2,
+    image: '/press/pr2.jpg',
+    title: "GPU cloud startup Boost Run to go public via de-SPAC merger",
+    description: "The merger is likely to entice retail investors who are keen to get in on the AI boom. Boost Run is one of a growing number of cloud infrastructure providers that’s dedicated to AI workloads, offering access to competitively-priced servers powered by Nvidia Corp.’s graphics processing units, which support high-performance training and inference. The merger is likely to entice retail investors who are keen to get in on the AI boom. Boost Run is one of a growing number of cloud infrastructure providers that’s dedicated to AI workloads, offering access to competitively-priced servers powered by Nvidia Corp.’s graphics processing units, which support high-performance training and inference. Turn on screen reader support To enable screen reader support, press ⌘+Option+Z To learn about keyboard shortcuts, press ⌘slashMaya Hernandez has joined the document.",
+    link: "https://siliconangle.com/2025/09/16/gpu-cloud-startup-boost-run-go-public-via-de-spac-merger/",
   }
 ];
 
