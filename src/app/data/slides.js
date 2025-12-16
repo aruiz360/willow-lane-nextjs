6fe983f37d4b207eee5446c0c3ee6616a3a57688
Andres Ruiz <a.ruiz@axiomaestudio.com>
2025-12-16 20:46:28 +0100

    Updated Logo, Icon, News Section

--- src/app/data/slides.js
+++ src/app/data/slides.js
@@ -12,6 +12,13 @@ const slides = [
     title: "GPU cloud startup Boost Run to go public via de-SPAC merger",
     description: "The merger is likely to entice retail investors who are keen to get in on the AI boom. Boost Run is one of a growing number of cloud infrastructure providers that’s dedicated to AI workloads, offering access to competitively-priced servers powered by Nvidia Corp.’s graphics processing units, which support high-performance training and inference. The merger is likely to entice retail investors who are keen to get in on the AI boom. Boost Run is one of a growing number of cloud infrastructure providers that’s dedicated to AI workloads, offering access to competitively-priced servers powered by Nvidia Corp.’s graphics processing units, which support high-performance training and inference.",
     link: "https://siliconangle.com/2025/09/16/gpu-cloud-startup-boost-run-go-public-via-de-spac-merger/",
+  },
+  {
+    id: 3,
+    image: '/press/pr3.jpg',
+    title: "Boost Run Expands AI Infrastructure Ecosystem with Additional GPU, Data Center, Financing, and Customer Partnerships as it Prepares to Merge with Willow Lane Acquisition Corp.",
+    description: "Boost Run, LLC, a rapidly growing provider of AI Cloud Infrastructure and high performance compute (HPC), and Willow Lane Acquisition Corp. (Nasdaq: WLAC), a publicly-traded special purpose acquisition company, today announced Boost Run's recent entry into a series of commercial agreements that collectively aim to expand and diversify Boost Run's access to next generation hardware, data center capacity power and capital, alongside significant new customer commitment. These partnerships underscore Boost Run's disciplined approach to profitable growth and strengthen visibility into long term capacity expansion.",
+    link: "https://siliconangle.com/2025/09/16/gpu-cloud-startup-boost-run-go-public-via-de-spac-merger/",
   }
 ];
 
--- src/app/press-releases/page.js
+++ src/app/press-releases/page.js
@@ -1,26 +1,10 @@
 import Carrousel from '../components/Carrousel'
+import slides from '../data/slides'
 
 export const metadata = {
     title: 'Investor Relations | Willow Lane Acquisition Corporation',
     description: 'Access important financial information and SEC filings for Willow Lane Acquisition Corporation.',
 }
-
-const slides = [
-  {
-    id: 1,
-    image: '/press/bloomberg.jpg',
-    title: "Boost Run Set to Go Public in US Via $614 Million SPAC Merger",
-    description: "AI cloud infrastructure company Boost Run LLC has reached an agreement to go public on the Nasdaq through a merger with Willow Lane Acquisition Corp.",
-    link: "https://www.bloomberg.com/news/articles/2025-09-16/boost-run-set-to-go-public-in-us-via-614-million-spac-merger?srnd=undefined&embedded-checkout=true",
-  },
-  {
-    id: 2,
-    image: '/press/pr2.jpg',
-    title: "GPU cloud startup Boost Run to go public via de-SPAC merger",
-    description: "The merger is likely to entice retail investors who are keen to get in on the AI boom. Boost Run is one of a growing number of cloud infrastructure providers that’s dedicated to AI workloads, offering access to competitively-priced servers powered by Nvidia Corp.’s graphics processing units, which support high-performance training and inference. The merger is likely to entice retail investors who are keen to get in on the AI boom. Boost Run is one of a growing number of cloud infrastructure providers that’s dedicated to AI workloads, offering access to competitively-priced servers powered by Nvidia Corp.’s graphics processing units, which support high-performance training and inference.",
-    link: "https://siliconangle.com/2025/09/16/gpu-cloud-startup-boost-run-go-public-via-de-spac-merger/",
-  }
-];
 
 export default function InvestorRelations() {
     return (
